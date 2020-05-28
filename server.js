ad1e2d3e071e35a97bb498c5c51241c6ebb07723
ben <s108652@ap.be>
2020-05-28 09:53:27 +0200

    crosywosy

--- server.js
+++ server.js
@@ -29,10 +29,14 @@ app.use(bodyParser.json());
 //   optionsSuccesStatus: 200
 // }
 
-app.use(function(req,res,next){
-  res.header("Acces-Control-Allow-Origin", "*");
-  res.header("Acces-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-});
+//CORS Middleware
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+  });
 
 //start application server on port 3000
 app.listen(3000, () => {
